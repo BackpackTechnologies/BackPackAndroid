15e0c387e72244b954190383e7d9bb7ea98d2253
ghebant <guillaume.hebant@epitech.eu>
2018-06-11 22:22:17 +0200

    test deploy

--- push_master.sh
+++ push_master.sh
@@ -2,4 +2,4 @@
 
 git config --global user.email "guillaume.hebant@epitech.eu"
 git config --global user.name "ghebant"
-git push https://$TRAVIS_TOKEN@github.com/ghebant/TravisAndroidTest HEAD:master
+git push https://$TRAVIS_TOKEN@github.com/backpacktechnologies/BackPackAndroid HEAD:master
